1b1b043a9a8b8ebc7f05eec37de207a27b9bdad8
Meteor <coopticks@outlook.com>
2025-02-10 02:47:12 +0000

    t

--- main.go
+++ main.go
@@ -4,17 +4,40 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 )
+
+const LLGOModuleIdentifyFile = "llpkg.cfg"
 
 func main() {
 	changes := os.Getenv("ALL_CHANGED_FILES")
 	if changes == "" {
 		panic("cannot find changes file!")
 	}
+
+	pathMap := map[string][]string{}
+
 	br := bufio.NewScanner(strings.NewReader(changes))
 
+	identifyFileCnt := 0
+
 	for br.Scan() {
-		fmt.Println(br.Text())
+		if strings.Contains(br.Text(), LLGOModuleIdentifyFile) {
+			identifyFileCnt++
+			if identifyFileCnt > 1 {
+				panic("only one module in single pr")
+			}
+		}
+		baseDir := filepath.Dir(br.Text())
+		file := filepath.Base(br.Text())
+		// build a file path
+		pathMap[baseDir] = append(pathMap[baseDir], file)
 	}
+
+	if identifyFileCnt == 0 {
+		panic("no identify file!")
+	}
+
+	fmt.Println(pathMap)
 }

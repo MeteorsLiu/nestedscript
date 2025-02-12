83651be61502b94908d95c6861871e7619236a96
Meteor <coopticks@outlook.com>
2025-02-12 08:26:36 +0000

    P

--- main.go
+++ main.go
@@ -61,8 +61,15 @@ func handle(path string, sc *Config) {
 		copyFile(match)
 	}
 	// export a local env in Go is impossible, so we use exec
-	exec.Command("export", fmt.Sprintf("LLCPPG_ABS_PATH=%s%s\n", sc.Package.Name, sc.Package.Version)).Run()
-	exec.Command("export", fmt.Sprintf("ARTIFACT_NAME=%s\n", absPath)).Run()
+	exec.Command("export", fmt.Sprintf("LLCPPG_ABS_PATH=%s%s", sc.Package.Name, sc.Package.Version)).Run()
+	exec.Command("export", fmt.Sprintf("ARTIFACT_NAME=%s", absPath)).Run()
+
+	env, err := os.OpenFile(os.Getenv("GITHUB_ENV"), os.O_APPEND, 0755)
+	must(err)
+
+	env.WriteString(fmt.Sprintf("LLCPPG_ABS_PATH=%s%s\n", sc.Package.Name, sc.Package.Version))
+	env.WriteString(fmt.Sprintf("LLCPPG_ABS_PATH=%s%s\n", sc.Package.Name, sc.Package.Version))
+	env.Close()
 
 }
 

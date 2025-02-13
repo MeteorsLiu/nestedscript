4a613319da3e3e828fe042b48520e9a4b9c97254
Meteor <coopticks@outlook.com>
2025-02-13 07:05:21 +0000

    P

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"maps"
 	"os"
 	"os/exec"
@@ -14,24 +13,6 @@ import (
 const LLGOModuleIdentifyFile = "llpkg.cfg"
 
 var currentSuffix = runtime.GOOS + "_" + runtime.GOARCH
-
-func copyFile(originalFile string) {
-	fileName := strings.TrimSuffix(originalFile, filepath.Ext(originalFile))
-
-	newFile, err := os.Create(fmt.Sprintf("%s_%s.go", fileName, currentSuffix))
-	must(err)
-	defer newFile.Close()
-
-	newFile.Write([]byte(fmt.Sprintf(`//go:build %s && %s
-`, runtime.GOOS, runtime.GOARCH)))
-
-	current, err := os.Open(originalFile)
-	must(err)
-	// remove old file
-	defer os.Remove(originalFile)
-
-	io.Copy(newFile, current)
-}
 
 func handle(path string, sc *Config) {
 	absPath, _ := filepath.Abs(path)
@@ -55,32 +36,25 @@ func handle(path string, sc *Config) {
 	cmd.Run()
 
 	localPath := filepath.Join(absPath, sc.Package.Name)
-	// done, rename all file, and upload to artifact
-	matches, _ := filepath.Glob(filepath.Join(localPath, "*.go"))
-
-	for _, match := range matches {
-		copyFile(match)
-	}
 
 	llpkgPath := filepath.Join(localPath, ".llpkg")
 	os.Mkdir(llpkgPath, 0755)
 
 	// be careful about the paths of llcppg config file here
 	// llcppg.cfg/symb.json is in absPath, while pub is in
-	os.Rename(
-		filepath.Join(absPath, "llcppg.cfg"),
-		filepath.Join(llpkgPath, fmt.Sprintf("llcppg_%s.cfg", currentSuffix)))
+
 	os.Rename(
 		filepath.Join(absPath, "llcppg.symb.json"),
-		filepath.Join(llpkgPath, fmt.Sprintf("llcppg_%s.symb.json", currentSuffix)))
+		filepath.Join(llpkgPath, "llcppg.symb.json"))
 	os.Rename(
 		filepath.Join(localPath, "llcppg.pub"),
-		filepath.Join(llpkgPath, fmt.Sprintf("llcppg_%s.pub", currentSuffix)))
+		filepath.Join(llpkgPath, "llcppg.pub"))
 
 	env, err := os.OpenFile(os.Getenv("GITHUB_ENV"), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	must(err)
 
-	env.WriteString(fmt.Sprintf("ARTIFACT_NAME=%s%s\n", sc.Package.Name, sc.Package.Version))
+	// prevent duplicate name
+	env.WriteString(fmt.Sprintf("ARTIFACT_NAME=%s%s_%s\n", sc.Package.Name, sc.Package.Version, currentSuffix))
 	env.WriteString(fmt.Sprintf("LLCPPG_ABS_PATH=%s\n", localPath))
 	env.Close()
 }
